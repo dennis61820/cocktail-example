1ee87f329e0634e8004777f3f5f38f939e687726
dennis61820 <danngit@hotmail.com>
2021-10-07 09:39:43 -0500

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus
+        explicabo suscipit commodi sequi, nostrum illum fuga omnis tenetur vero
+        repellendus at dolorum quisquam ratione voluptates cumque eos nulla
+        ducimus cum mollitia iusto, provident, ad magni eius deleniti. Tempore
+        omnis perspiciatis laboriosam.
+      </p>
+    </section>
+  );
+};
+
+export default About;
